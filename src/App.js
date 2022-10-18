be1bd7ba177a0b45b25fa817a4acd5cb8ce29d2a
Olha Bilkovska <91874687+ollka-ever@users.noreply.github.com>
2022-10-18 15:46:40 +0300

    link to github is added

--- src/App.js
+++ src/App.js
@@ -2,5 +2,17 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <footer>
+        <a
+          href="https://github.com/ollka-ever/react-weather"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Open-source project
+        </a>
+      </footer>
+    </div>
+  );
 }
